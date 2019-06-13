22c2d5c182b72c914a333b3ba4fdc5ff8f2ce74c
adityasreeram007 <42211360+adityasreeram007@users.noreply.github.com>
2019-06-13 22:19:52 +0530

    Add files via upload

--- telegram flask/main.py
+++ telegram flask/main.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session
+import os
 app=Flask(__name__)
-@app.route('/telegram',methods = ['POST'])
+@app.route('/')
 def telegram():
-    return render_template('telegram.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')       
+    else:
+        return render_template('telegram.html')
 @app.route('/login', methods = ['POST'])
 def login():
     error = None
@@ -10,19 +14,19 @@ def login():
     if request.method == 'POST':
         if (request.form["username"] == 'admin' or request.form["password"] == 'admin'):
             #print(request.form['username'])
-            error = 'Invalid username or password. Please try again!'
-            return render_template('login.html', error = error)
+            session['logged_in']=True
         else:
-            flash('You were successfully logged in')
-            return redirect(url_for('telegram'))    
-    return render_template('login.html', error = error)
-@app.route('/groups',methods = ['POST'])
+            flash('Wrong password')
+    return telegram()   
+    #return render_template('login.html', error = error)
+@app.route('/groups')
 def group():
     return render_template('groups.html')
 # @app.route('/home')
 # def home():
 #     return render_template('telegram.html')
 if __name__=='__main__':
+    app.secret_key=os.urandom(12)
     app.run(debug=True)
 
 
